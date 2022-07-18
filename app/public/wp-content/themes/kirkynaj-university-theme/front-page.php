ecda7361e08942a02ed1a62d92a6522f938a1b55
Kirk Jann V. Garcia <janngarcia24@gmail.com>
2022-07-19 06:21:16 +0800

    Section 7 (30-35)

--- app/public/wp-content/themes/kirkynaj-university-theme/front-page.php
+++ app/public/wp-content/themes/kirkynaj-university-theme/front-page.php
@@ -16,26 +16,46 @@
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           
           <?php 
+            $today = date('Ymd');
             $homepageEvents = new WP_Query(array(
-              'post_per_page' => 2,
-              'post_type' => 'event'
+              'posts_per_page' => 2,
+              'post_type' => 'event',
+              'meta_key' => 'event_date',
+              'orderby' => 'meta_value_num',
+              'order' => 'ASC',
+              'meta_query' => array(
+                array(
+                  'key' => 'event_date',
+                  'compare' => '>=',
+                  'value' => $today,
+                  'type' => 'numeric'
+
+                ),
+              )
             ));
             while($homepageEvents->have_posts()) {
               $homepageEvents->the_post(); ?>
                <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                  <span class="event-summary__month">Apr</span>
-                  <span class="event-summary__day">02</span>
+                  <span class="event-summary__month"><?php 
+                    $eventDate = new DateTime(get_field('event_date'));
+                    echo $eventDate->format('M')
+                  ?></span>
+                  <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                  <p><?php if (has_excerpt()) {
+                     echo get_the_excerpt();  
+                    } else {
+                      echo wp_trim_words(get_the_content(), 18);
+                    }?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
                 </div>
               </div>
           <?php  }
           ?>   
           
-          <p class="t-center no-margin"><a href="<?php echo site_url('/blogs') ?>" class="btn btn--blue">View All Events</a></p>
+          <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
       <div class="full-width-split__two">
@@ -57,7 +77,11 @@
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                    <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                    <p><?php if (has_excerpt()) {
+                     echo get_the_excerpt();  
+                    } else {
+                      echo wp_trim_words(get_the_content(), 18);
+                    }?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                   </div>
                 </div>
           <?php  } wp_reset_postdata();
